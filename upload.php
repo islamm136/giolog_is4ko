0f0065f8b8dc51f7e76d8a90d5fc5fd24acee227
islam136 <islamnizamov25@gmail.com>
2025-03-26 12:37:50 +0600

    wrrr

--- upload.php
+++ upload.php
@@ -1,4 +1,10 @@
 <?php
+
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     $uploadDir = 'uploads/';
     if (!is_dir($uploadDir)) {
